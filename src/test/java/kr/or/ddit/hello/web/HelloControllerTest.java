ab01fdbc5858e7da0a1302cbb41b287f9465e461
2434hin <2434hin@gmail.com>
2019-09-17 10:36:53 +0900

    spring controller 테스트 	. static import를 통한 소스 간결화 		. 이클립스 java - editor - favorite 	. mockMvc.andExpect 		. status() 		. view 		. model()

--- src/test/java/kr/or/ddit/hello/web/HelloControllerTest.java
+++ src/test/java/kr/or/ddit/hello/web/HelloControllerTest.java
@@ -1,42 +1,20 @@
 package kr.or.ddit.hello.web;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.ModelAndView;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration("classpath:kr/or/ddit/config/spring/servlet-context.xml")	// controller scan : servlet-context.xml
-@WebAppConfiguration	// 스프링 컨테이너를 구성할 web기반 application context로 구성
-public class HelloControllerTest {
+import kr.or.ddit.config.test.WebTestConfig;
+
+public class HelloControllerTest extends WebTestConfig{
+
 	
-	// controller를 테스트 하기 위해 필요한 것 2가지
-	// applicationContext : 스프링 컨테이너
-	// MockMvc : dispatcherServlet (applicationContext객체를 통해 설정) 
-	
-	// 주입하려고하는 필드의 타입과 일치할 경우 이름과 관계없이 주입
-	// 만약 주입하려고 하는 필드의 타입과 스프링 빈 중에 타입이 일치하는 빈이 2개이상 존재할 경우 에러
-	@Autowired	
-	private WebApplicationContext context;
-	
-	private MockMvc mockMvc;
-	
-	@Before
-	public void setup() {
-		mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
-	}
 
 	// server(tomcat)가 없는 환경에서 테스트 가능하다.
 	@Test
@@ -44,7 +22,7 @@ public class HelloControllerTest {
 		/***Given***/
 		
 		/***When***/
-		MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/hello/hello.do").param("userId", "brown")).andReturn();
+		MvcResult mvcResult = mockMvc.perform(get("/hello/hello.do").param("userId", "brown")).andReturn();
 		ModelAndView mav = mvcResult.getModelAndView();
 		String msg = (String) mav.getModel().get("msg");
 		String userId = (String) mav.getModelMap().get("userId");
@@ -58,6 +36,17 @@ public class HelloControllerTest {
 		assertEquals("hello/hello", mav.getViewName());
 		assertEquals("hello, World", msg);
 		assertEquals("brown_helloController", userId);
+	}
+	
+	@Test
+	public void helloTest2() throws Exception {
+		mockMvc.perform(get("/hello/hello.do").param("userId", "sally"))
+			   .andExpect(status().isOk())
+			   .andExpect(view().name("hello/hello"))
+//			   .andExpect(model().attributeExists("msg"))
+//			   .andExpect(model().attributeExists("userId"))
+			   .andExpect(model().attribute("msg", "hello, World"))
+			   .andExpect(model().attribute("userId", "sally_helloController"));
 	}
 
 }
