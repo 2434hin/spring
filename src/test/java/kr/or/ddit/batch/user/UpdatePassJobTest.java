67b1b8302f4d8c98353afe21b7743581310588f4
2434hin <2434hin@gmail.com>
2019-09-26 16:55:31 +0900

    spring batch  . spring-mybatis 모듈 적용  . 사용 비밀번호 암호화 배치

---
+++ src/test/java/kr/or/ddit/batch/user/UpdatePassJobTest.java
@@ -0,0 +1,34 @@
+package kr.or.ddit.batch.user;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:kr/or/ddit/config/spring/context-batch.xml",
+								   "classpath:kr/or/ddit/config/spring/context-datasource.xml"})
+public class UpdatePassJobTest {
+	
+	@Autowired
+	private JobLauncherTestUtils jobLauncherTestUtils;
+
+	@Test
+	public void updatePassJobTest() throws Exception {
+		/***Given***/
+
+		/***When***/
+		JobExecution jobExecution = jobLauncherTestUtils.launchJob();
+		
+		/***Then***/
+		assertEquals(ExitStatus.COMPLETED, jobExecution.getExitStatus());
+
+	}
+
+}
